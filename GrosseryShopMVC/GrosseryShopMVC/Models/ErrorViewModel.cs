38a7e45054fcb0016df8d4ac4b9807404752ba22
Raman Vakula <vakularomans@gmail.com>
2023-02-16 13:00:40 +0300

    Set up an empty project

---
+++ GrosseryShopMVC/GrosseryShopMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GrosseryShopMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
